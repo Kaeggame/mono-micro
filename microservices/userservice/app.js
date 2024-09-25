5692ed6c90e3385c60d0fad0e974d6379569dfa0
Flamby <66632621+Kaeggame@users.noreply.github.com>
2024-09-25 15:16:01 +0200

    Microservice up

--- microservices/userservice/app.js
+++ microservices/userservice/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const http = require('http');
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 
 // Simulons une base de données en mémoire
-let users = [{ id: 1, name: 'John Doe' }];
+let users = [{ id: 1, name: 'Albert' },
+            { id: 2, name: 'Michel' }
+];
 
 // Routes pour la gestion des utilisateurs
 app.get('/users', (req, res) => {
@@ -16,6 +19,25 @@ app.post('/users', (req, res) => {
   const user = { id: users.length + 1, name: req.body.name };
   users.push(user);
   res.json(user);
+});
+
+app.get('/users/produit/:id', (req, res) => {
+  const user = users.find((user) => user.id === Number(req.params.id));
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+  http.get('http://localhost:3002/products/' + user.id, (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+    response.on('end', () => {
+      user.products = JSON.parse(data);
+      res.json(user);
+    });
+  }).on('error', (err) => {
+    res.status(500).json({ error: 'Error fetching products' });
+  });
 });
 
 app.listen(port, () => {
